dfcffe09db16480ef0f5c4409042ca449b38e737
Tabitha Sin <tabitha.sin@gmail.com>
2018-02-07 20:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(feet) {
 }
 
 function ternaryCheckCity(city) {
-  city === "NYC" ? "Ok, sounds good." : "No go."
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
