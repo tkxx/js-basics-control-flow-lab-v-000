aeb6a19466d3648c5fb1012fbde23f056d47835a
Tabitha Sin <tabitha.sin@gmail.com>
2018-02-07 20:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-    return = "Thank you so much.";
+    return "Thank you so much.";
     break;
     case 'not as generous':
-    return = "Thank you.";
+    return "Thank you.";
     break;
     default:
-    return = "Bye.";
+    return "Bye.";
     break;
   }
 }
