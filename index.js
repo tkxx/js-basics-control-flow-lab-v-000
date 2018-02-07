7ab251e1b4a44e2699aab22fc1c12ef5c9f1402b
Tabitha Sin <tabitha.sin@gmail.com>
2018-02-07 20:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+function scuberGreetingForFeet {
+  if (feet <= 400) {
+    return "This one is on me!"
+  }
+  else if { (feet > 2000) && (feet < 2500)
+    return "I will gladly take your thirty bucks."
+  }
+  else if {(feet > 2500)
+    return "No can do."
+  }  
+  }
+  }
+}
